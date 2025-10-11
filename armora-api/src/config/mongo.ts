52f5b7e23340b984c53596a537326e9d2ede4ce1
Atherban <atherbandas@gmail.com>
2025-10-11 16:20:07 +0530

    Old Backend Setup Removed

--- armora-api/src/config/mongo.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-export async function connectMongo(uri: string) {
-  await mongoose.connect(uri);
-  mongoose.connection.on('connected', () => console.log('mongo ok'));
-  mongoose.connection.on('error', (e) => console.error('mongo err', e));
-}
